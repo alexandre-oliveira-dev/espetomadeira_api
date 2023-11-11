719b8dbf7330a016ac380668a7e0e8dfc94b4765
Alexandre_oliveiradev <alexandredev.frontend@gmail.com>
2023-11-11 08:24:57 -0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,9 @@ function app() {
   });
   app.use(cors({origin:'*'}))
   app.use(route);
-  app.listen(3333, ()=> console.log('api online'));
-/*   app.listen(process.env.PORT, ()=> console.log('api online'));
- */}
+  /*   app.listen(3333, ()=> console.log('api online'));*/
+  app.listen(process.env.PORT, ()=> console.log('api online'));
+ }
 app();
 
 
